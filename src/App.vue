e14dc99f45f492d49bb6acbe9d033d2434d6de52
John Louie Navales <louienvls09@gmail.com>
2025-02-01 11:00:39 +0800

    feat: setup Vue Router, Pinia and some components

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div class="text-red-400">
+        TEXT HERE
+    </div>
+
+    <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
