997e0e961baa3ea200f21f482d0d37373d6a7571
John Louie Navales <louienvls09@gmail.com>
2025-02-20 10:46:09 +0800

    refactor: restructure Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,18 @@
-<template>
-<v-app>
-
-    <v-app :theme="$vuetify.theme.global.name.value">
-        <v-btn>
-            <ThemeSwitcher />
-        </v-btn>
-     <Carousel/>
-     <br>
-     <Navigation/>
- </v-app>
-
-
-</v-app>
-</template>
-
 <script setup>
 import Carousel from "../landingpage/Carousel.vue";
 import Navigation from "../landingpage/Navigation.vue";
 import ThemeSwitcher from "@/components/ThemeSwitcher.vue";
 </script>
 
-
-<style scoped>
-
-</style>
+<template>
+    <v-app>
+        <v-app :theme="$vuetify.theme.global.name.value">
+            <v-btn>
+            <ThemeSwitcher />
+            </v-btn>
+            <Carousel/>
+            <br>
+            <Navigation/>
+        </v-app>
+    </v-app>
+</template>
