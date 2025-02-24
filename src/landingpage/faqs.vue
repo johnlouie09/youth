03730f28acc3428c549dc9174007dc0f1ea04fdc
John Louie Navales <louienvls09@gmail.com>
2025-02-25 07:44:17 +0800

    feat: update FAQs section with new questions and answers

--- src/landingpage/faqs.vue
+++ src/landingpage/faqs.vue
@@ -3,7 +3,7 @@
         <div class="gradient-text">
             <h1>Frequently Asked Questions</h1>
         </div>
-        <v-expansion-panels>
+        <!-- <v-expansion-panels>
             <v-expansion-panel
                 v-for="i in 3"
                 :key="i"
@@ -13,6 +13,61 @@
                 <v-expansion-panel-title class="faq-title">
                     Item {{ i }}
                 </v-expansion-panel-title>
+                <v-expansion-panel-text class="faq-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels> -->
+        <v-expansion-panels>
+            <v-expansion-panel>
+                <v-expansion-panel-title class="faq-title">
+                    What is YOUTH Platform?
+                </v-expansion-panel-title>
+                <v-divider></v-divider>
+                <v-expansion-panel-text class="faq-text">
+                    The YOUTH Platform (Youth-Oriented Unified Transparency Hub) is an online system design to enhance transparency and engagement between SK officials and the youth in selected barangays of Iriga CIty. It provides information about SK keader, their advocacies, platforms, and accomplishments, helping young citizens make informed decisions and actively participate in local governance.
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels>
+        <v-expansion-panels>
+            <v-expansion-panel>
+                <v-expansion-panel-title class="faq-title">
+                    What is Sangguaniang Kabataan?
+                </v-expansion-panel-title>
+                <v-divider></v-divider>
+                <v-expansion-panel-text class="faq-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels>
+        <v-expansion-panels>
+            <v-expansion-panel>
+                <v-expansion-panel-title class="faq-title">
+                    What is SK Reform Act of 2015?
+                </v-expansion-panel-title>
+                <v-divider></v-divider>
+                <v-expansion-panel-text class="faq-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels>
+        <v-expansion-panels>
+            <v-expansion-panel>
+                <v-expansion-panel-title class="faq-title">
+                    What is NYC?
+                </v-expansion-panel-title>
+                <v-divider></v-divider>
+                <v-expansion-panel-text class="faq-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels>
+        <v-expansion-panels>
+            <v-expansion-panel>
+                <v-expansion-panel-title class="faq-title">
+                    What is LYDO?
+                </v-expansion-panel-title>
+                <v-divider></v-divider>
                 <v-expansion-panel-text class="faq-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </v-expansion-panel-text>
