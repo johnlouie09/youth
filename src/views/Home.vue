3607117cffa1cfe4d5f158b7cfdcb7094c0f2ec4
John Louie Navales <louienvls09@gmail.com>
2025-02-25 09:43:09 +0800

    refactor: simplify <v-app> structure and improve component imports

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,15 @@
 <script setup>
 import Carousel from "../landingpage/Carousel.vue";
 import Navigation from "../landingpage/Navigation.vue";
-import ThemeSwitcher from "@/components/ThemeSwitcher.vue";
-import faqs from '../landingpage/faqs.vue'
+import Faqs from '../landingpage/faqs.vue';
 </script>
 
 <template>
-<v-app>
-
-    <v-app :theme="$vuetify.theme.global.name.value">
-
-     <Carousel/>
-     <br>
-     <Navigation/>
+    <v-app :theme="$vuetify.theme.name">
+        <Carousel/>
         <br>
-        <faqs/>
- </v-app>
-
-</v-app>
+        <Navigation/>
+        <br>
+        <Faqs/>
+    </v-app>
 </template>
