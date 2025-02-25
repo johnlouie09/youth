f5fc7cabcd00e662dc6036086f5e35c3503a19de
SharuHaru <harveygonzaga222@gmail.com>
2025-02-25 14:27:02 +0800

    UI Update: Analytics and Suggestions SubComponents

---
+++ src/components/adminComponents/subcomponents/dashboard/Analytics.vue
@@ -0,0 +1,78 @@
+<script setup>
+</script>
+
+<template>
+    <div class="analytics-box">
+        <nav>
+            <ul>
+                <li class="active">DAILY</li>
+                <li>MONTHLY</li>
+                <li>YEARLY</li>
+            </ul>
+        </nav>
+
+        <section>
+
+        </section>
+    </div>
+</template>
+
+<style scoped>
+    .analytics-box {
+        font-family: 'Inter', sans-serif;
+
+        height: 75%;
+        width: 100%;
+        
+        background-color: #2C2C2C;
+        border: 1px solid #373737;
+        border-radius: .5rem;
+        box-shadow: 0px 15px 15px 0px rgba(0, 0, 0, 0.25);
+
+        padding: 1.5rem 3rem;
+
+        display: flex;
+        gap: 1.5rem;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    nav {
+        width: 90%;
+    }
+
+    ul {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 4rem;
+        width: 100%;
+
+    }
+
+    ul li {
+        width: 100%;
+        padding: .5rem 1rem;
+        border-bottom: 4px solid #3F3F3F;
+        font-weight: bold;
+
+        text-align: center;
+
+        font-size: 1.25rem;
+    }
+    
+    .active {
+        border-bottom: 4px solid #3772FF;
+
+    }
+
+    .analytics-box section {
+        width: 100%;
+        height: 100%;
+        background-color: black;
+    }
+
+
+
+
+</style>
---
+++ src/components/adminComponents/subcomponents/dashboard/Suggestions.vue
@@ -0,0 +1,134 @@
+<script setup>
+</script>
+
+<template>
+    <div class="suggestions-box">
+        <h2>YOUTH SUGGESTIONS NOTIFICATIONS</h2>
+        <ul>
+            <li class="active">UNREAD</li>
+            <li>ALL</li>
+        </ul>
+        <div class="messages">
+            <article v-for="n in 9">
+                <h3>Youth Skills Development Program</h3>
+                <p>Implement monthly workshops on practical skills such as basic coding, graphic design, entrepreneurship, and financial literacy. This will help equip ...........</p>
+                <span>1 hr ago</span>
+            </article>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    /* Make scrollbar small */
+    ::-webkit-scrollbar {
+        width: 6px;  /* Set the scrollbar width */
+        height: 6px; /* Set the scrollbar height (for horizontal scrollbars) */
+    }
+
+    /* Scrollbar track (background) */
+    ::-webkit-scrollbar-track {
+        background: #1e1e1e;  /* Dark background */
+        border-radius: 10px;
+    }
+
+    /* Scrollbar thumb (draggable part) */
+    ::-webkit-scrollbar-thumb {
+        background: #555;  /* Gray thumb */
+        border-radius: 10px;
+    }
+
+    /* Scrollbar thumb on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #888;  /* Lighter gray on hover */
+    }
+
+    .suggestions-box {
+        height: 90%;
+        width: 100%;
+
+        background-color: #2C2C2C;
+        border: 1px solid #373737;
+        border-radius: .5rem;
+        box-shadow: 0px 15px 15px 0px rgba(0, 0, 0, 0.25);
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+
+        padding: 36px;
+        padding-bottom: 16px;  
+        
+        gap: 24px;
+    }
+
+    h2 {
+        font-weight: bolder;
+        font-size: 1.25rem;
+        width: 100%;
+        text-align: center;
+    }
+
+    ul {
+        width: 100%;
+        display: flex;
+        gap: 1rem;
+    }
+
+    ul li {
+        padding: 12px 28px;
+        border-bottom: 2px solid #3F3F3F;
+        font-weight: bold;
+    }
+
+    .active {
+        border-bottom: 2px solid #3772FF;
+    }
+
+    .messages {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        height: 500px;
+        width: 100%;
+        overflow-y: scroll;
+    }
+
+    .messages article{
+        background-color: #202020;
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+        gap: 5px;
+        
+
+        padding: 1.5rem;
+        border-radius: .5rem;
+
+        
+    }
+
+    article h3 {
+        font-size: 1.25rem;
+        font-weight: bolder;
+    }
+
+    article p, article span {
+        font-size: .75rem;
+    }
+
+    article span {
+        width: 100%;
+        text-align: right;
+        font-weight: bold;
+    }
+
+
+
+
+
+</style>
