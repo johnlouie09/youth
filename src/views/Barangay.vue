9ec7734d2bcbe5db60d749ebf6ddf1d68c87e848
SharuHaru <harveygonzaga222@gmail.com>
2025-03-11 18:43:49 +0800

    UI Update: Baarangay View

--- src/views/Barangay.vue
+++ src/views/Barangay.vue
@@ -1,18 +1,14 @@
 <template>
-    <v-container :theme="$vuetify.theme.global.name.value" fluid class="barangay-main ma-0 pa-0">
-        <div class="relative flex justify-center items-center overflow-hidden">
-            <img class="w-full object-fill" :src="barangayHall" alt="" style="height: 350px;" />
-            <div class="absolute left-0 top-1/2 transform -translate-y-1/2" style="margin-left: 100px; scale: 2.5;">
-                <img class="w-24 h-auto" src="/Logoseal.svg" alt="logo" />
+    <v-container :theme="$vuetify.theme.global.name.value" fluid class="barangay-main">
+        
+        <div class="dp-barangay elevation-20">
+            <img class="logo w-80 h-auto" src="/Logoseal.svg" alt="logo" />
+            <div>
+                BARANGAY {{ barangay.name.toUpperCase() }}
             </div>
-            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-bold">
-                {{ `Barangay ${barangay.name}` }}
-            </div>
-            <div class="absolute right-0 top-1/2 transform -translate-y-1/2" style="margin-right: 100px; scale: 2.5;">
-                <img class="w-24 h-auto" src="/Logoseal.svg" alt="logo" />
-            </div>
+            <img class="logo w-80 h-auto" src="/Logoseal.svg" alt="logo" />
         </div>
-        <Carousel />
+        <Announcements></Announcements>
         <Cards></Cards>
         <Achievements :achievements="barangay.achievements" />
     </v-container>
@@ -20,13 +16,13 @@
 
 <script>
 import barangayHall from '/San_Francisco_Barangay_Hall.JPG?url';
-import Carousel from '@/components/barangayPage/Carousel.vue';
+import Announcements from '@/components/barangayPage/Announcements.vue';
 import Cards from '@/components/barangayPage/Cards.vue';
 import Achievements from '@/components/barangayPage/Achievements.vue';
 
 export default {
     components: {
-        Carousel,
+        Announcements,
         Cards,
         Achievements
     },
@@ -99,42 +95,49 @@ export default {
                 },
                 achievements: [
                     {
-                        img: '/public/Sangguniang_Kabataan_logo.svg',
+                        img: '1.jpg',
                         title: "Basketball",
                         subtitle: "Kung walang nilaga sana tayo nalang...",
-                        info: "I'm a thing. But, like most politicians, he promised more than he could deliver."
+                        info: "Basketbfall is not just a game—it’s a way to bring communities together, foster teamwork, and inspire young people to achieve their dreams. In our barangay, basketball courts serve as a hub for socializing and learning important life lessons such as discipline, resilience, and the value of hard work."
                     },
                     {
-                        img: '/public/Sangguniang_Kabataan_logo.svg',
+                        img: '2.jpg',
                         title: "Volleyball",
                         subtitle: "Bilog ang bola, bilog din ang mundo...",
-                        info: "I'm just a ball, but I bring people together."
+                        info: "Volleyball energizes our community by promoting physical fitness and cooperation among neighbors. This sport has been a cornerstone of community events, where participants learn about trust, coordination, and the importance of maintaining a healthy lifestyle, while also having fun and building lasting friendships."
                     },
                     {
-                        img: '/public/Sangguniang_Kabataan_logo.svg',
+                        img: '3.jpg',
                         title: "Volleyball",
                         subtitle: "Bilog ang bola, bilog din ang mundo...",
-                        info: "I'm just a ball, but I bring people together."
+                        info: "Volleyball energizes our community by promoting physical fitness and cooperation among neighbors. This sport has been a cornerstone of community events, where participants learn about trust, coordination, and the importance of maintaining a healthy lifestyle, while also having fun and building lasting friendships."
                     },
                     {
-                        img: '/public/Sangguniang_Kabataan_logo.svg',
+                        img: '4.jpg',
                         title: "Volleyball",
                         subtitle: "Bilog ang bola, bilog din ang mundo...",
-                        info: "I'm just a ball, but I bring people together."
+                        info: "Volleyball energizes our community by promoting physical fitness and cooperation among neighbors. This sport has been a cornerstone of community events, where participants learn about trust, coordination, and the importance of maintaining a healthy lifestyle, while also having fun and building lasting friendships."
                     },
                     {
-                        img: '/public/Sangguniang_Kabataan_logo.svg',
+                        img: '5.jpg',
                         title: "Volleyball",
                         subtitle: "Bilog ang bola, bilog din ang mundo...",
-                        info: "I'm just a ball, but I bring people together."
+                        info: "Volleyball energizes our community by promoting physical fitness and cooperation among neighbors. This sport has been a cornerstone of community events, where participants learn about trust, coordination, and the importance of maintaining a healthy lifestyle, while also having fun and building lasting friendships."
                     },
                     {
-                        img: '/public/Sangguniang_Kabataan_logo.svg',
+                        img: '6.jpg',
                         title: "Volleyball",
                         subtitle: "Bilog ang bola, bilog din ang mundo...",
-                        info: "I'm just a ball, but I bring people together."
+                        info: "Volleyball energizes our community by promoting physical fitness and cooperation among neighbors. This sport has been a cornerstone of community events, where participants learn about trust, coordination, and the importance of maintaining a healthy lifestyle, while also having fun and building lasting friendships."
+                    },
+                    {
+                        img: '7.jpg',
+                        title: "Volleyball",
+                        subtitle: "Bilog ang bola, bilog din ang mundo...",
+                        info: "Volleyball energizes our community by promoting physical fitness and cooperation among neighbors. This sport has been a cornerstone of community events, where participants learn about trust, coordination, and the importance of maintaining a healthy lifestyle, while also having fun and building lasting friendships."
                     }
                 ]
+
             },
             barangayHall
         };
@@ -145,27 +148,66 @@ export default {
 
 
 <style scoped>
-.gradient-text {
-    background-image: linear-gradient(to right, #3772FF 40%, #DF2935 50%, #FDCA40 60%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-}
-.font-bold {
-    background-image: linear-gradient(45deg, #3772FF 2%, #DF2935 20%, #FDCA40 35%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    font-weight: bold;
-}
-
 .barangay-main {
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
     gap: 10rem;
     background: #000000; /* fallback for old browsers */
     background-image: linear-gradient(45deg, #0e0e0e, #363636); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    /* background-image: linear-gradient(to right top, #18122b, #1e1732, #241d3a, #2a2241, #302849, #342c4e, #382f53, #3c3358, #3e355c, #403760, #423a64, #443c68); */
     background-position: center;
     background-attachment: fixed;
+    margin: 0;
+    padding: 0;
+    padding-bottom: 5rem;
+}
 
+.dp-barangay {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 45vh;
+    border-radius: 0 0 2rem 2rem;
+    overflow: hidden; /* Ensure pseudo-element doesn't overflow */
+}
+
+/* Pseudo-element with background image */
+.dp-barangay::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(../../public/San_Francisco_Barangay_Hall.JPG);
+    background-position: center;
+    background-repeat: repeat;
+    background-size: auto;
+    opacity: 0.5; /* Adjust opacity as needed */
+    z-index: 1;
+}
+
+.dp-barangay div {
+    font-size: 3rem;
+    font-weight: 900;
+    background-image: linear-gradient(
+        45deg,
+        #0533a0 10%,
+        #ffffff 20%,
+        #DF2935 40%,
+        #d4d4d4 60%,
+        #FDCA40 80%,
+        #d4d4d4 100%
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+/* Ensure content is on top of the pseudo-element */
+.dp-barangay > * {
+    z-index: 2;
 }
 </style>
