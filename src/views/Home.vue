910a32e7ee7f579ccfa9d5c1a508ca53a1c47375
SharuHaru <harveygonzaga222@gmail.com>
2025-03-12 14:58:03 +0800

    For Merge

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,44 @@
-<script setup>
-import Carousel from "../landingpage/Carousel.vue";
-import Navigation from "../landingpage/Navigation.vue";
-import ThemeSwitcher from "@/components/ThemeSwitcher.vue";
-</script>
-
 <template>
-    <v-app>
-        <v-app :theme="$vuetify.theme.global.name.value">
-            <v-btn>
-            <ThemeSwitcher />
-            </v-btn>
-            <Carousel/>
-            <br>
-            <Navigation/>
-        </v-app>
-    </v-app>
-</template>
+    <v-container
+      fluid
+      class="home-main"
+      :class="{ 'dark-gradient': theme.global.name.value === 'dark' }"
+    >
+      <Carousel />
+      <BarangaysNavigation />
+      <Faqs />
+    </v-container>
+  </template>
 
-<style scoped>
-    * {
-        font-family: 'Inter', sans-serif;
-    }
-</style>
+  <script setup>
+  import Carousel from "../landingpage/Hero.vue";
+  import Faqs from "@/landingpage/Faqs.vue";
+  import BarangaysNavigation from "../landingpage/BarangaysNavigation.vue";
+  import { useTheme } from 'vuetify';
+  
+  const theme = useTheme();
+  </script>
+
+  <style scoped>
+  .home-main {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5rem;
+    padding: 0;
+    padding-top: 2rem;
+    padding-bottom: 4rem;
+    margin: 0;
+    /* No gradient by default */
+    background-position: center;
+    background-attachment: fixed;
+    background: linear-gradient(45deg, rgba(253,202,64,0.76234243697479) 14%, rgba(255,255,255,1) 32%, rgba(55,114,255,0.5970763305322129) 53%, rgba(255,255,255,1) 72%, rgba(223,41,53,0.5830707282913166) 90%);
+  }
+  
+  /* Only add the gradient if dark mode is active */
+  .dark-gradient {
+    background-image: linear-gradient(45deg, #0e0e0e, #363636);
+  }
+  </style>
