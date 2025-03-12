cd86468f734e7138ce925fc2a4091682217203d1
SharuHaru <harveygonzaga222@gmail.com>
2025-03-12 14:40:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
-<script setup>
-// import Preloader from "./components/Preloader.vue";
-import Footer from "./components/Footer.vue";
-import ThemeSwitcher from './components/ThemeSwitcher.vue';
+<script>
+import Preloader from "./components/Preloader.vue";
+import Footer from "./components/landingPage/Footer.vue";
+import NavigationBar from "./components/landingPage/NavigationBar.vue";
+export default {
+  components: {
+    NavigationBar,
+    Preloader,
+    Footer
+  }
+};
 </script>
 
 <template>
-
-<!--    <Preloader />-->
-    <RouterView/>
-
+  <v-app>
+    <NavigationBar></NavigationBar>
+    <v-main>
+      <v-container class="pa-0" fluid >
+        <Preloader />
+        <RouterView />
+      </v-container>
+    </v-main>
+    <Footer/>
+  </v-app>
 </template>
 
 <style scoped>
-
 </style>
+  
