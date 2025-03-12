167d4d617cbae3b14b6176b554ba1ad1f7a0f522
SharuHaru <harveygonzaga222@gmail.com>
2025-03-12 15:06:26 +0800

    Update for Merge

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SanFrancisco from "../views/poblacion/SanFrancisco.vue"; // Path to the new page
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,100 +10,24 @@ const router = createRouter({
       alias: ['/home']
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../Login.vue'),
-    },
-    {
-
-
       path: '/contact',
-      name: 'contact',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Contact.vue'),
     },
-    // {
-    //   path: '/SanFrancisco',
-    //   name: 'SanFrancisco',
-    //   component: SanFrancisco,
-    // },
-
-
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: () => import('../components/404.vue'),
+    },
     {
       path: '/san-francisco',
       name: 'San Francisco',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/poblacion/SanFrancisco.vue'),
     },
     {
-      path: '/mountain/perpetual',
+      path: '/barangay',
       name: 'perpetual',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/mountain/PerpetualHelp.vue'),
+      component: () => import('../views/Barangay.vue'),
     },
-
-
-
-    // ADMINS ROUTER
-    {
-      path: '/admin',
-      name: 'admin',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Admin.vue'),
-      children: [
-        {
-          path: 'dashboard',
-          component: () => import('../components/adminComponents/Dashboard.vue'),
-        },
-        {
-          path: 'officials', 
-          component: () => import('../components/adminComponents/Officials.vue'),
-        },
-        {
-          path: 'announcements',  
-          component: () => import('../components/adminComponents/Announcements.vue'),
-        },
-        {
-          path: 'achievements',
-          component: () => import('../components/adminComponents/Achievements.vue'),
-        },
-        {
-          path: 'settings', 
-          component: () => import('../components/adminComponents/Settings.vue'),
-        },
-        {
-          path: 'notices', 
-          component: () => import('../components/adminComponents/Notices.vue'),
-        },
-        {
-          path: 'officials/johndoe',
-          component: () => import('../components/adminComponents/subcomponents/officials/EditingOfficial.vue'),
-        },
-        // DEMO ->
-        {
-          path: 'demo/message-dialog',
-          component: () => import('../components/adminComponents/dialogDemo/MessageDialog.vue')
-        },
-        {
-          path: 'demo/confirm-dialog',
-          component: () => import('../components/adminComponents/dialogDemo/ConfirmDialog.vue')
-        },
-        {
-          path: 'demo/demoo',
-          component: () => import('../components/adminComponents/dialogDemo/Toast.vue')
-        },
-
-        // <-- DEMO
-      ]
-    }
   ],
 })
 
--- youth.sql
+++ youth.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2025 at 08:44 PM
+-- Generation Time: Mar 05, 2025 at 10:38 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
